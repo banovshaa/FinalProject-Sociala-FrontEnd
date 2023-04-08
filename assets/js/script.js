c9bfc20f044217ef534f8efff9b56835b2bba7c5
banovshaa <banovshaea@code.edu.az>
2023-04-08 17:20:00 +0400

    1st

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:5,
+        loop:true,
+        dots:false,
+        video:true,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:6000,
+        infinite:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+    });
+    $('.users-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+  });
