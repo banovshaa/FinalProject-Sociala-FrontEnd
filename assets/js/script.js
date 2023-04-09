933a3a35f9c12dafd32616b6a47aca8eedf36f8d
banovshaa <banovshaea@code.edu.az>
2023-04-10 00:37:06 +0400

    3rd

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,31 +64,34 @@ $(document).ready(function(){
         speed: 600,
         slidesToShow: 1,
         slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
       });
-              
-  });
+    $('.single-product-image').slick({
+      slidesToShow: 1,
+      arrows: true,
+      fade: true,
+      draggable: false,
+      swipe: false,
+      asNavFor: ".multiple-product-image"
+    });
+    $('.multiple-product-image').slick({
+      vertical: true,
+      infinite:true,
+      autoplay: true,
+      autoplaySpeed:8000,
+      slidesToShow: 4,
+      arrows: false,
+      asNavFor:".single-product-image",
+      focusOnSelect:true
+    });
+    $(".sb-range-slider").slider({
+        range: true,
+        min: 0,
+        max: 753,
+        values: [80, 320],
+        slide: function(event, ui) {
+            $("#amount").val("£" + ui.values[0] + " - £" + ui.values[1]);
+        }
+    });
+    $("#amount").val("£" + $(".sb-range-slider").slider("values", 0) +
+        " - £" + $(".sb-range-slider").slider("values", 1));   
+});
