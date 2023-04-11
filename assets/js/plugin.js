833066f71438e8745c75d405cd93f0269df7a600
banovshaa <banovshaea@code.edu.az>
2023-04-12 01:46:33 +0400

    6th

---
+++ assets/js/plugin.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:5,
+        loop:true,
+        dots:false,
+        video:true,
+        responsiveClass:true,
+        autoplay:true,
+        autoplayTimeout:6000,
+        infinite:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+    });
+    $('.users-slider').slick({
+        dots: false,
+        arrows:false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+    $('.shop-slider').slick({
+        dots: true,
+        arrows:true,
+        infinite: true,
+        speed: 600,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      });
+    $('.single-product-image').slick({
+      slidesToShow: 1,
+      arrows: true,
+      fade: true,
+      draggable: false,
+      swipe: false,
+      asNavFor: ".multiple-product-image"
+    });
+    $('.multiple-product-image').slick({
+      vertical: true,
+      infinite:true,
+      autoplay: true,
+      autoplaySpeed:8000,
+      slidesToShow: 4,
+      arrows: false,
+      asNavFor:".single-product-image",
+      focusOnSelect:true
+    });
+    $(".sb-range-slider").slider({
+        range: true,
+        min: 0,
+        max: 753,
+        values: [80, 320],
+        slide: function(event, ui) {
+            $("#amount").val("£" + ui.values[0] + " - £" + ui.values[1]);
+        }
+    });
+    $("#amount").val("£" + $(".sb-range-slider").slider("values", 0) +
+        " - £" + $(".sb-range-slider").slider("values", 1));   
+});
