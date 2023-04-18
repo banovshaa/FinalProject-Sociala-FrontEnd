86adf75d95a4963692c4c43381039b3bef276fad
banovshaa <banovshaea@code.edu.az>
2023-04-18 05:02:41 +0400

    12th

--- assets/js/plugin.js
+++ assets/js/plugin.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+      $('.acc-container .acc:nth-child(1) .acc-head').addClass('active');
+      $('.acc-container .acc:nth-child(1) .acc-content').slideDown();
+      $('.acc-head').on('click', function() {
+          if($(this).hasClass('active')) {
+            $(this).siblings('.acc-content').slideUp();
+            $(this).removeClass('active');
+          }
+          else {
+            $('.acc-content').slideUp();
+            $('.acc-head').removeClass('active');
+            $(this).siblings('.acc-content').slideToggle();
+            $(this).toggleClass('active');
+          }
+      });  
     $('.users-slider').slick({
         dots: false,
         arrows:false,
